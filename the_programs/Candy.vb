6e8da5ee62c50313a68a6365711f6b265efb6f01
=Israel Montano <montano.israel@icloud.com>
2014-11-06 08:44:44 -0500

    I added a function and a sub to the form

--- the_programs/Candy.vb
+++ the_programs/Candy.vb
@@ -1,3 +1,13 @@
 ﻿Public Class frmCandy
 
+    Function CalculateTheTotal(ByVal intOz As Integer, ByVal dblPrize As Double)
+        Dim dblTotal As Double
+        dblTotal = intOz * dblPrize
+        Return dblTotal
+    End Function
+
+    Private Sub Calculate(ByRef strCandy As String, ByVal intOz As Integer, ByVal dblPrize As Double)
+
+    End Sub
+
 End Class
