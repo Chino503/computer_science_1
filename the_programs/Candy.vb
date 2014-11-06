32d550058382bebd52f34e2f1d93b89d1662598b
Israel Montano <montano.israel@icloud.com>
2014-11-06 18:18:20 -0500

    I just added the label

--- the_programs/Candy.vb
+++ the_programs/Candy.vb
@@ -7,7 +7,8 @@
     End Function
 
     Private Sub Calculate(ByRef strCandy As String, ByVal intOz As Integer, ByVal dblPrize As Double)
-
+	Dim dblCost As Integer
+	lblAnswer.Text = intOz & " ounces and they cost " & dblCost
     End Sub
 
 End Class
