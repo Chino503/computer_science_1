ad318662069b4a3292697283927abb67a5d9380b
=Israel Montano <montano.israel@icloud.com>
2014-11-12 07:44:14 -0500

    I finished the sortNumber form and turned it in

--- the_programs/SortNumbers.vb
+++ the_programs/SortNumbers.vb
@@ -1,4 +1,6 @@
-﻿Public Class frmSortNumbers
+﻿'Chino Montano 11/12/14 | 701 sortNumber Excercise
+
+Public Class frmSortNumbers
 
     Private Sub sortNum(ByRef intNum1 As Integer, ByRef intNum2 As Integer)
         Dim intTemp As Integer
@@ -6,9 +8,8 @@
             intTemp = intNum1
             intNum1 = intNum2
             intNum2 = intTemp
-
         End If
-        lblSort.Text = "The low number is " & intTemp & " and the high number is " & intNum2
+
     End Sub
 
     Private Sub btnSort_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSort.Click
@@ -16,22 +17,21 @@
         Dim intNum1 As Integer
         Dim strNum2 As String
         Dim intNum2 As Integer
-        For intI As Integer = 1 To 2
 
-            If intI = 1 Then
-                strNum1 = InputBox("Please enter your 1st number", "Number Sorting")
-                strNum2 = InputBox("Please enter your 2nd number", "Number Sorting")
-                If Int16.TryParse(strNum1, intNum1) = False Or Int16.TryParse(strNum2, intNum2) = False Then
-                    MessageBox.Show("Please enter numberic values")
-                    Exit Sub
-                Else
-                    intNum1 = Int16.TryParse(strNum1, intNum1)
-                    intNum2 = Int16.TryParse(strNum2, intNum2)
-                    sortNum(intNum1, intNum2)
-                End If
-            End If
 
-        Next
+
+        strNum1 = InputBox("Please enter your 1st number", "Number Sorting")
+        strNum2 = InputBox("Please enter your 2nd number", "Number Sorting")
+        If Int16.TryParse(strNum1, intNum1) = False Or Int16.TryParse(strNum2, intNum2) = False Then
+            lblSort.Text = Nothing
+            MessageBox.Show("Please enter numberic values")
+            Exit Sub
+        Else
+            ' intNum1 = Int16.TryParse(strNum1, intNum1)
+            ' intNum2 = Int16.TryParse(strNum2, intNum2)
+            sortNum(intNum1, intNum2)
+            lblSort.Text = "The low number is " & intNum1 & " and the high number is " & intNum2
+        End If
 
     End Sub
 End Class
