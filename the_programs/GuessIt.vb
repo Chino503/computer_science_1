079f5babcb8865d383fbfff3afbf4615fdfd137c
=Israel Montano <montano.israel@icloud.com>
2014-11-12 08:35:29 -0500

    Finished all the forms for 701

--- the_programs/GuessIt.vb
+++ the_programs/GuessIt.vb
@@ -1,31 +1,35 @@
-﻿Public Class frmGuessIt
+﻿'Chino Montano 11/12/14 | 701 GuessIt Excercise
 
-    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+Public Class frmGuessIt
 
-        Dim intGuess As Integer
+    Private Sub Hint(ByVal intGuess As Integer)
         Dim gen As New Random()
         Static intRandomNum As Integer = gen.Next(1, 51) 'number between 1 - 50
         Static intTries As Integer
         intTries += 1
+        If intGuess < intRandomNum Then
+            lblAnswer.Text = "Too low"
+        ElseIf intGuess > intRandomNum Then
+            lblAnswer.Text = "Too High"
+        Else
+            lblAnswer.Text = "Congrats!!"
+            MessageBox.Show("Congrats!! You guessed it," & vbCrLf & " it took you " & intTries & " tries to guess it.")
+        End If
 
+        If intGuess > 50 Then
+            MessageBox.Show("Please try guessing numbers between 1 and 50")
+        ElseIf intGuess < 1 Then
+            MessageBox.Show("Please try guessing numbers between 1 and 50")
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim intGuess As Integer
         Try
             intGuess = Convert.ToInt32(txtGuess.Text)
-
-            If intGuess < intRandomNum Then
-                lblAnswer.Text = "Too low"
-            ElseIf intGuess > intRandomNum Then
-                lblAnswer.Text = "Too High"
-            Else
-                lblAnswer.Text = "Congrats!!")
-                MessageBox.Show("Congrats!! You guessed it," & vbCrLf & " it took you " & intTries & " tries to guess it.")
-            End If
-
-            If intGuess > 50 Then
-                MessageBox.Show("Please try guessing numbers between 1 and 50")
-            ElseIf intGuess < 1 Then
-                MessageBox.Show("Please try guessing numbers between 1 and 50")
-            End If
-
+            Hint(intGuess)
         Catch ex As Exception
             MessageBox.Show("Please enter integer values for your guess")
         End Try
