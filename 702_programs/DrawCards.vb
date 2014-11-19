e82d8c960dd5c88096b69329d72e063ff0293dad
=Israel Montano <montano.israel@icloud.com>
2014-11-19 08:00:41 -0500

    I finished the DrawCards form and I have debugged it

--- 702_programs/DrawCards.vb
+++ 702_programs/DrawCards.vb
@@ -1,3 +1,43 @@
-﻿Public Class frmDrawCards
+﻿'Chino Montano
+'11/18/14   
+'702 DrawCards Excercise 8
 
+Public Class frmDrawCards
+
+    Private Sub drawCards(ByRef intCardCount() As Integer, ByVal intTimes As Integer)
+        Dim gen As New Random
+        Dim intCardDraws As Integer
+        For intI As Integer = 0 To intTimes - 1
+            intCardDraws = gen.Next(2, 11) 'random number between 2 and 1
+            intCardCount(intCardDraws - 2) += 1
+        Next
+    End Sub
+
+    Private Sub showCards(ByVal intCardCount() As Integer)
+        For intI As Integer = 0 To intCardCount.Length - 1
+            Dim intCardNum As Integer = 1
+            intCardNum += 1
+            lblAnswer.Text += "Card #" & intCardNum & " was drawn " & intCardCount(intI) & " times" & vbCrLf
+        Next
+    End Sub
+
+    Private Sub btnDraw_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnDraw.Click
+        Dim intCardCount(8) As Integer
+        Dim intTimes As Integer
+        lblAnswer.Text = Nothing
+        Try
+            intTimes = Convert.ToInt32(txtDraws.Text)
+            If intTimes < 0 Then
+                MessageBox.Show("You cannot draw negative times, please try again")
+            ElseIf intTimes = 0 Then
+                MessageBox.Show("You cannot draw 0 times, please try again")
+            Else
+                drawCards(intCardCount, intTimes)
+                showCards(intCardCount)
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Please enter numberic whole numbers")
+        End Try
+
+    End Sub
 End Class
--- 702_programs/My Project/Application.Designer.vb
+++ 702_programs/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global._702_programs.frmGPAArray
+            Me.MainForm = Global._702_programs.frmDrawCards
         End Sub
     End Class
 End Namespace
