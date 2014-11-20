0e30cdd72fe177665760075cb8e3f752acec2ed5
=Israel Montano <montano.israel@icloud.com>
2014-11-20 08:05:44 -0500

    I had to debugged the RandomNum form

--- 702_programs/RandomNums.vb
+++ 702_programs/RandomNums.vb
@@ -4,10 +4,10 @@
 
 Public Class frmRandomNums
 
-    Dim intRandNumArr(9), intLow, intHigh As Integer
+    Dim intRandNumArr(9) As Integer
 
     'randomizes numbers between 1 and 100 and stores it in an array
-    Private Sub genRandNum(ByRef intRandNumArr() As Integer)
+    Private Sub genRandNum()
         Dim gen As New Random
         Dim randNum As Integer
         For intI As Integer = 0 To 9
@@ -17,47 +17,41 @@ Public Class frmRandomNums
     End Sub
 
     'display random numbers in array
-    Private Sub showNums(ByVal intRandNumArr() As Integer)
+    Private Sub showNums()
         For Each intI As Integer In intRandNumArr
             lstNums.Items.Add(intI)
         Next
     End Sub
 
-    'gets high or low nums from array
-    Private Sub getNums(ByVal intRandNumArr() As Integer, ByRef intLow As Integer, ByRef intHigh As Integer)
-        Array.Sort(intRandNumArr)
-        intLow = intRandNumArr(0)
-        Array.Reverse(intRandNumArr)
-        intHigh = intRandNumArr(0)
-    End Sub
-
     'Generator button
     Private Sub btnGenerator_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnGenerator.Click
         lstNums.Items.Clear()
-        genRandNum(intRandNumArr)
-        showNums(intRandNumArr)
+        genRandNum()
+        showNums()
     End Sub
 
     'Show Low button
     Private Sub btnLow_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnLow.Click
-        getNums(intRandNumArr, intLow, intHigh)
+        Dim intLow As Integer
+        Array.Sort(intRandNumArr)
+        intLow = intRandNumArr(0)
         If intLow = Nothing Then
             MessageBox.Show("Make sure you generate random numbers first")
         Else
             lblShowNum.Text = "Low number is " & intLow
         End If
-
     End Sub
 
     'Show High button
     Private Sub btnHigh_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnHigh.Click
-        getNums(intRandNumArr, intLow, intHigh)
-        If intLow = Nothing Then
+        Dim intHigh As Integer
+        Array.Reverse(intRandNumArr)
+        intHigh = intRandNumArr(0)
+        If intHigh = Nothing Then
             MessageBox.Show("Make sure you generate random numbers first")
         Else
             lblShowNum.Text = "High Number is " & intHigh
         End If
-
     End Sub
 
 End Class
