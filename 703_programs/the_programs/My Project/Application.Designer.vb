b1c049fb66ac3929d6d93b646b8504fc392482fb
=Israel Montano <montano.israel@icloud.com>
2014-12-04 07:26:52 -0500

    Idk what this are. I just forgot to commit them

--- 703_programs/the_programs/My Project/Application.Designer.vb
+++ 703_programs/the_programs/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.the_programs.frmMyInitials
+            Me.MainForm = Global.the_programs.frmRemoveWord
         End Sub
     End Class
 End Namespace
