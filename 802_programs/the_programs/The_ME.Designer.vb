ff7e1aff8a669020da3f50cd2fafec7424c3c132
=Israel Montano <montano.israel@icloud.com>
2014-12-08 08:49:09 -0500

    I added a new form called frmMe and I have not finished it

---
+++ 802_programs/the_programs/The_ME.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMe
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ShowMeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HideMeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CloseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lblGraphics = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ShowMeToolStripMenuItem, Me.HideMeToolStripMenuItem, Me.CloseToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(284, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ShowMeToolStripMenuItem
+        '
+        Me.ShowMeToolStripMenuItem.Name = "ShowMeToolStripMenuItem"
+        Me.ShowMeToolStripMenuItem.Size = New System.Drawing.Size(68, 20)
+        Me.ShowMeToolStripMenuItem.Text = "Show Me"
+        '
+        'HideMeToolStripMenuItem
+        '
+        Me.HideMeToolStripMenuItem.Name = "HideMeToolStripMenuItem"
+        Me.HideMeToolStripMenuItem.Size = New System.Drawing.Size(67, 20)
+        Me.HideMeToolStripMenuItem.Text = "Hide Me "
+        '
+        'CloseToolStripMenuItem
+        '
+        Me.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem"
+        Me.CloseToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
+        Me.CloseToolStripMenuItem.Text = "Close"
+        '
+        'lblGraphics
+        '
+        Me.lblGraphics.Location = New System.Drawing.Point(15, 46)
+        Me.lblGraphics.Name = "lblGraphics"
+        Me.lblGraphics.Size = New System.Drawing.Size(253, 192)
+        Me.lblGraphics.TabIndex = 1
+        '
+        'frmMe
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.lblGraphics)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmMe"
+        Me.Text = "Me"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents ShowMeToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HideMeToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CloseToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents lblGraphics As System.Windows.Forms.Label
+End Class
---
+++ 802_programs/the_programs/The_ME.vb
@@ -0,0 +1,12 @@
+﻿Public Class frmMe
+
+    Private Sub drawMe()
+        Dim surface As Graphics = lblGraphics.CreateGraphics
+        Dim thePen As New Pen(Color.Black, 3)
+        surface.DrawLine(thePen, 0, 0, 150, 50)
+    End Sub
+
+    Private Sub ShowMeToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ShowMeToolStripMenuItem.Click
+        drawMe()
+    End Sub
+End Class
