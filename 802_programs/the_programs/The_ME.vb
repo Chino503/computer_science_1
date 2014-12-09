6555cc29d3f86de1222ce0c01f68faec75e91fad
=Israel Montano <montano.israel@icloud.com>
2014-12-09 07:55:43 -0500

    I finished the frmMe

--- 802_programs/the_programs/The_ME.vb
+++ 802_programs/the_programs/The_ME.vb
@@ -1,31 +1,45 @@
-﻿Public Class frmMe
+﻿'Chino Montano 
+'12/09/14
+'802 Me excercise
 
-    Private Sub drawMe()
+Public Class frmMe
+
+    Private Sub drawMe(ByVal strDrawOrClear As String)
         Dim surface As Graphics = lblGraphics.CreateGraphics
+
+        'pens
         Dim thePen As New Pen(Color.Black, 3)
+
+        'brushes
         Dim salmonBrush As New SolidBrush(Color.DarkSalmon)
         Dim mouthBrush As New SolidBrush(Color.White)
-        Dim theEyesBrush As New SolidBrush(Color.Black)
+        Dim blackBrush As New SolidBrush(Color.Black)
+
         Dim nosePoints() As Point = {New Point(120, 75), New Point(110, 100), New Point(130, 105)} 'the nose line points
 
-        surface.FillEllipse(salmonBrush, 70, 20, 120, 140) 'The head
-        surface.FillPie(mouthBrush, 100, 90, 40, 50, 0, 180) 'The Mouth
+        If strDrawOrClear = "draw" Then
+            surface.FillEllipse(salmonBrush, 70, 20, 120, 140) 'The head
+            surface.FillRectangle(blackBrush, 55, 0, 150, 10) 'hat
+            surface.FillRectangle(blackBrush, 80, 0, 100, 50) 'hat
 
-        'the eyes
-        surface.FillEllipse(theEyesBrush, 100, 60, 15, 15)
-        surface.FillEllipse(theEyesBrush, 130, 60, 15, 15)
+            'the eyes
+            surface.FillEllipse(blackBrush, 100, 60, 15, 15)
+            surface.FillEllipse(blackBrush, 130, 60, 15, 15)
 
-        'the nose
-        surface.DrawLines(thePen, nosePoints)
+            surface.DrawLines(thePen, nosePoints) 'the nose
 
+            surface.FillPie(mouthBrush, 100, 90, 40, 50, 0, 180) 'The Mouth
+        ElseIf strDrawOrClear = "clear" Then
+            surface.Clear(Me.BackColor)
+        End If
     End Sub
 
     Private Sub ShowMeToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ShowMeToolStripMenuItem.Click
-        drawMe()
+        drawMe("draw")
     End Sub
 
     Private Sub HideMeToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles HideMeToolStripMenuItem.Click
-
+        drawMe("clear")
     End Sub
 
     Private Sub CloseToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CloseToolStripMenuItem.Click
